2dcc6845ff269631a7d9f998a059775639d33fe0
Artem Sokolov <arsokolov@ptsecurity.com>
2024-10-04 16:30:06 +0300

    added python

---
+++ python/shell.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+
+let
+  pythonPackages = pkgs.python312Packages;
+in
+pkgs.mkShell {
+  buildInputs = with pythonPackages; [
+    python
+    venvShellHook
+  ];
+  venvDir = "./venv";
+  postVenvCreation = ''
+    unset SOURCE_DATE_EPOCH
+  '';
+  postShellHook = ''
+    unset SOURCE_DATE_EPOCH
+  '';
+}
