61fe86a06cb611be89c2e5f722ad160a3e96e099
Artem Sokolov <arsokolov@ptsecurity.com>
2025-05-17 14:12:59 +0300

    fix

--- javascript/default.nix
+++ javascript/default.nix
@@ -1,15 +1,1 @@
-{ python312Packages, ... }:
-
-with python312Packages;
-buildPythonApplication {
-  pname = "aoc";
-  version = "2023";
-  pyproject = true;
-  nativeBuildInputs = [
-    more-itertools
-    networkx
-    setuptools
-    wheel
-  ];
-  src = ./.;
-}
+{ ... }: { }
--- javascript/shell.nix
+++ javascript/shell.nix
@@ -3,6 +3,7 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     nodejs
+    electron
   ];
   postShellHook = ''
     unset SOURCE_DATE_EPOCH
